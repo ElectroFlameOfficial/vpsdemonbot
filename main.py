2a7b0cc1818ad4d1473a8afb7cb5b535d8c8637a
ElectroFlame <asemnada991@gmail.com>
2020-09-08 20:14:31 -0400

    project files push (for heroku)

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="v!")
+
+LIST = ("WIP.")
+
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Game(name="with fellow list grinders"))
+  print('It works')
+
+@client.command()
+async def list(ctx):
+    await ctx.send(LIST)
+
+
+@client.command(pass_context=True)
+@commands.has_role('moderator')
+async def repeat(ctx, content):
+    await ctx.send(content)
+
+client.run("NzUzMDE4ODA1MjQ4MTk2NzQx.X1gFaQ.ZDXpWKniQXfWZyeFyqrOynx8TSE")
+
+keep_alive.keep_alive()
+
+# Finally, login the bot
+bot.run(os.environ.get('TOKEN'), bot=True, reconnect=True)
